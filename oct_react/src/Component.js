fa6c78392fbabf4a58bdde548b6e8d1b3d320d25
huong09 <huongyangyang9199@gmail.com>
2022-11-16 15:07:40 +0700

    message

---
+++ oct_react/src/Component.js
@@ -0,0 +1,8 @@
+function Component(){
+    return(
+        <div>
+            <p>Xin chào ReactJS, hiển thị HTML sử dụng class Component</p>
+        </div>
+    );
+}
+export default Component;
--- oct_react/src/index.js
+++ oct_react/src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Component from './Component';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Component></Component>
   </React.StrictMode>
 );
 
